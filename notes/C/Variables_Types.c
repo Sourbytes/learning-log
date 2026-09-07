0eb5870a763f32b6b834b88f5afe8ae7f2ba76db
sourbytes <soura.html@gmail.com>
2026-09-07 22:25:16 +0530

    C folder with files added

---
+++ notes/C/Variables_Types.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+
+int main()
+{
+    int a = 3;
+    float b = 4.5;
+    double c = 5.25;
+    int i = 0;
+    fl
+}
---
+++ notes/C/py_to_c.c
@@ -0,0 +1,37 @@
+// #include <stdio.h>
+// main() {
+//   printf("Hello world\n");
+// }
+
+// #include <stdio.h>
+// int main() {
+//     printf("Hello world\n");
+//     printf("Answer %d\n",17);
+//     printf("Name %s\n","Sarah");
+// }
+
+// #include <stdio.h>
+// int main() {
+//     int usf, euf;
+//     printf("Enter US Floor\n");
+//     scanf("%d", &usf);
+//     euf = usf - 1;
+//     printf("EU Floor %d\n", euf);
+// }
+
+// #include <stdio.h>
+// int main() {
+//     char name[100];
+//     printf("Enter name\n");
+//     scanf("%100s", name);
+//     printf("Hello %s\n", name);
+// }
+
+
+// #include <stdio.h>
+// int main() {
+//   	int i;
+//   	for(i=0; i<5; i++) {
+//     printf("%d\n", i);
+//     }
+// }
